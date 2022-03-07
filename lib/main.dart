213af1f588ec25ded36d8f2b0d2058557be25e7f
Yosep Novento Nugroho <noventon@mamiteam.com>
2022-03-08 06:36:40 +0700

    create layouting static dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/images/primary.png',
+                  width: 140,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Vento Deco',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Software Engineer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: greyColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/images/item1.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item2.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item3.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item4.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item5.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item6.png',
+                      width: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: RaisedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                              height: 290,
+                              color: whiteColor,
+                              padding: EdgeInsets.symmetric(
+                                vertical: 50,
+                              ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Update Photo',
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 12,
+                                  ),
+                                  Text(
+                                    'You are only able to change\nthe picture profile once',
+                                    style: TextStyle(
+                                      color: greyColor,
+                                      fontSize: 18,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Container(
+                                    width: 224,
+                                    height: 55,
+                                    child: RaisedButton(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                      color: orangeColor,
+                                      onPressed: () {},
+                                      child: Text(
+                                        'Continue',
+                                        style: TextStyle(
+                                          color: whiteColor,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
